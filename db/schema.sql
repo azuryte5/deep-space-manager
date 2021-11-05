f2beb196fb901cb346fec1256ceb14987a40ace1
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-11-05 12:14:37 -0400

    done seeding

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS Employees;
+USE business;
+
 DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS department;
@@ -12,7 +13,7 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary DECIMAL (8,0) NOT NULL,
     department_id INTEGER,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
@@ -22,6 +23,6 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
-    manager_id INTEGER,
+    manager_id INTEGER REFERENCES employees(id),
     CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,15 +4,52 @@ VALUES
 (2, "security"),
 (3, "enginnering"),
 (4, "science"),
-(5, "medical");
+(5, "medical"),
+(6, "Intellegence");
 
 INSERT INTO roles (id, title, salary, department_id)
 VALUES
 (1, "commanding officer" , 100000, 1),
 (2, "first officer", 80000, 1),
-(3, "lieutenant commander", 75000, 2);
+(3, "lieutenant commander", 75000, 4),
+(4, "lieutenant commander", 75000, 2),
+(5, "lieutenant", 65000, 5),
+(6, "lieutenant junior", 55000, 5),
+(7, "lieutenant junior", 55000, 3),
+(8, "Chief of Operations", 85000, 3),
+(9, "Constable", 60000, 2),
+(10, "Simple Tailor", 40000, 6),
+(11, "Exobotanist", 50000, 4),
+(12, "chief medical officer", 50000, 5),
+(13, "engineer", 45000, 3),
+(14, "security personnel", 35000, 2),
+(15, "ensign", 35000, 1);
+
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-("Benjamin", "Sisko", 1, 0),
-("Kira", "Nerys", 2, 1);
+("Benjamin", "Sisko", 1, NULL),
+("Kira", "Nerys", 2, 1),
+("Jadzia", "Dax", 3, 1),
+("Worf", "Son of Mogh", 4, 1),
+("Julian", "Bashir", 5, 1),
+("Ezri", "Dax", 6, 5),
+("Nog", "Son of Rom", 7, 8),
+("Miles", "O'Brien", 8, 1),
+("Odo", "Changeling", 9, 4),
+("Elim", "Garak", 10, NULL),
+("Keiko", "O'Brien", 11, 3),
+("Simon", "Tarses", 12, 5),
+("Michael", "Eddington", 4, 1),
+("Phillipa", "Matthias", 12, 5),
+("Karan", "Adabwe", 13, 7),
+("Masoud", "Ahzed", 13, 7),
+("Natalia", "Aponte", 14, 9),
+("Dalia", "Parks", 14, 9),
+("Gerda", "Roness", 15, 2),
+("Jose", "Chavez", 15, 2);
+
+
+
+
+
